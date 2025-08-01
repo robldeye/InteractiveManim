cb30df039cee9cf57fb2b0dd016dccd115d2cf21
Robert Deyeso <robert-deyeso@uiowa.edu>
2025-07-31 20:11:37 -0500

    actually NExT demo

--- manim_util.py
+++ manim_util.py
@@ -22,6 +22,7 @@ class Integral(Scene):
             y_length=8,
             axis_config={"include_numbers": True},
         ).to_edge(RIGHT)
+        axes_labels = axes.get_axis_labels()
         
         f_graph = axes.plot(
             self.f, 
@@ -59,27 +60,22 @@ class Integral(Scene):
         )
         self.wait()
 
-        self.add(axes, f_graph, f_riemann)
+        self.play(
+            FadeIn(axes),
+            FadeIn(f_graph),
+            FadeIn(f_riemann)
+        )
         self.wait()
-
-        # dx_prefix = MathTex(r"dx =")
-        # dx_decimal = DecimalNumber(
-        #     dx_value.get_value(),
-        #     num_decimal_places=2,
-        # ).next_to(dx_prefix, RIGHT)
-        # dx_decimal.add_updater(lambda m: m.set_value(dx_value.get_value()))
-        # dx_label = VGroup(dx_prefix, dx_decimal).arrange(RIGHT)
-        # dx_label.next_to(title, DOWN).align_to(title, LEFT)
 
         self.play(
             b_value.animate.set_value(self.b),
-            run_time = 3
+            run_time = 2
         )
         self.wait(2)
 
         self.play(
             dx_value.animate.set_value(0.01),
-            run_time = 4
+            run_time = 3
         )
 
         # self.play(
